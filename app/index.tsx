f0753a3157d3a188519db1bd14e2f5e1f100872c
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-05-29 10:45:03 -0300

    start

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+import "@/global.css";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello World 2</Text>
+    </View>
+  );
+}
