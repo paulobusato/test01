03d3904f773db4c89b41eb31fedb08dc28b3f81a
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-06-05 21:45:06 -0300

    commit

--- components/NameCard.tsx
+++ components/NameCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card, Text } from "react-native-paper";
+import {Card, IconButton, Text} from "react-native-paper";
 import {Href, useRouter} from "expo-router";
 
 export interface NameCardProps {
@@ -8,20 +8,28 @@ export interface NameCardProps {
   route: Href;
 }
 
-const NameCard = ({ name, route }: NameCardProps) => {
+const NameCard = ({name, route}: NameCardProps) => {
   const router = useRouter()
 
   return (
-    <Card
-      mode="outlined"
-      onPress={() => router.push(route)}
-      contentStyle={{ padding: 8 }}
-      style={{ marginBottom: 8 }}
-    >
-      <Card.Content>
-        <Text variant="bodyMedium">{name}</Text>
-      </Card.Content>
-    </Card>
+      <Card
+          mode="outlined"
+          onPress={() => {
+          }}
+          contentStyle={{padding: 8}}
+          style={{marginBottom: 8}}
+      >
+        <Card.Content className="flex-row items-center justify-between">
+          <Text variant="bodyMedium" style={{fontWeight: "bold"}}>
+            {name}
+          </Text>
+          <IconButton
+              icon="pencil"
+              size={24}
+              onPress={() => router.push(route)}
+          />
+        </Card.Content>
+      </Card>
   );
 };
 
