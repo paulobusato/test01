9b3e4f96e304a6575c72b48a4ab22279ac79d2ba
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-06-05 21:58:27 -0300

    commit

--- app/sessao/ListSessaoScreen.tsx
+++ app/sessao/ListSessaoScreen.tsx
@@ -1,14 +1,23 @@
 import SearchInput from "@/components/SearchInput";
 import SessaoCard from "@/components/SessaoCard";
-import sessoes from "@/constants/SessaoList";
-import React from "react";
+import React, {useEffect} from "react";
 import { FlatList, View } from "react-native";
 
 import { FAB, useTheme } from "react-native-paper";
+import {useAppDispatch, useAppSelector} from "@/store/hooks";
+import {fetchSessoes} from "@/store/slices/sessaoSlice";
 
 const ListSessaoScreen = () => {
   const theme = useTheme();
+  const dispatch = useAppDispatch();
+
   const [text, setText] = React.useState("");
+
+  const { sessoes } = useAppSelector((state) => state.sessao);
+
+  useEffect(() => {
+    dispatch(fetchSessoes());
+  }, [dispatch]);
 
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
