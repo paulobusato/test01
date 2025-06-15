b629d63f8e6ed1f0d5ee51ff6ea6e078dc30f1f5
Paulo Henrique Busato <paulo.busato2@gmail.com>
2025-06-15 15:28:08 -0300

    commit

--- app/atividade/ListAtividadeScreen.tsx
+++ app/atividade/ListAtividadeScreen.tsx
@@ -2,9 +2,13 @@ import React, {useEffect} from "react";
 import {useAppSelector, useAppDispatch} from '@/store/hooks';
 import ListScreen from "@/components/ListScreen";
 import {fetchAtividades} from "@/store/slices/atividadeSlice";
+import {useLocalSearchParams, useRouter} from "expo-router";
+import {updateSessao} from "@/store/slices/sessaoSlice";
 
 const ListAtividadeScreen = () => {
   const dispatch = useAppDispatch();
+  const params: { id: string } = useLocalSearchParams();
+  const router = useRouter();
 
   const {atividades} = useAppSelector((state) => state.atividade);
 
@@ -12,11 +16,22 @@ const ListAtividadeScreen = () => {
     dispatch(fetchAtividades());
   }, [dispatch]);
 
+  const handleClick = (atividade: string) => {
+    dispatch(updateSessao({
+      id: params.id,
+      data: {
+        atividade: atividade,
+      }
+    }));
+    router.back();
+  }
+
   return (
       <ListScreen
           fabLabel={"Atividade"}
           data={atividades}
           route={"/atividade/EditAtividadeScreen"}
+          onClick={handleClick}
       />
   );
 };
